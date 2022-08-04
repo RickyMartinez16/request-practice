81c73786acc4b8011466aedd80b7db5d845565a5
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-04 10:39:00 -0400

    main

--- Making_requests-_Assessment_-Jest--Ricky_Martinez-Solution/src/main.js
+++ Making_requests-_Assessment_-Jest--Ricky_Martinez-Solution/src/main.js
@@ -1,15 +1,45 @@
 const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5000";
 
-function index() {}
+function index() {
+  return axios
+    .get(BASE_URL + "/constellations")
+    .then((response) => {
+      console.log(response)
+    })
+}
 
-function create(body) {}
+function create(body) {
+  return axios
+    .post(`${BASE_URL}/constellations/${body}`)
+    .then((response) => {
+      console.log(response.data)
+    })
+}
 
-function show(id) {}
+function show(id) {
+  return axios
+    .get(`${BASE_URL}/constellations/${id}`)
+    .then((response) => {
+      console.log(response.data.id)
+    })
+}
 
-function update(id, body) {}
+function update(id, body) {
+  return axios
+    .put(`${BASE_URL}/constellations/${id}`, body)
+    .then((response) => {
+      console.log(response.data)
+    })
+}
 
-function destroy(id) {}
+function destroy(id) {
+  return axios
+    .delete(`${BASE_URL}/constellations/${id}`)
+    .then((response) => {
+      console.log(response.data)
+    })
+}
 
 module.exports = {
   index,
